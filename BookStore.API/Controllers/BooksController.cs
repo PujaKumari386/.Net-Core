7e1e687aaeb352d5c55ec83b0ce5ad4d6644a9a3
github_Puja <pujasaharsa386@gmail.com>
2023-04-27 15:54:53 +0530

    first commit

---
+++ BookStore.API/Controllers/BooksController.cs
@@ -0,0 +1,56 @@
+﻿using BookStore.API.Models;
+using BookStore.API.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            var books = await _bookRepository.GetAllBooksAsync();
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBookById([FromRoute]int id)
+        {
+            var books = await _bookRepository.GetBookByIdAsync(id);
+            if (books == null)
+            {
+                return NotFound();
+            }
+            return Ok(books);
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> AddNewBook([FromBody] BookModel bookModel)
+        {
+            var id = await _bookRepository.AddBookAsync(bookModel);
+            return CreatedAtAction(nameof(GetBookById), new { id = id, controller = "books" }, id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBook([FromBody] BookModel bookModel, [FromRoute] int id)
+        {
+            await _bookRepository.UpdateBookAsync(id, bookModel);
+            return Ok();
+        }
+
+    }
+}
---
+++ BookStore.API/Repository/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Repository
+{
+    public interface IBookRepository
+    {
+        Task<List<BookModel>> GetAllBooksAsync();
+        Task<BookModel> GetBookByIdAsync(int bookId);
+        Task<int> AddBookAsync(BookModel bookModel);
+        Task UpdateBookAsync(int bookId, BookModel bookModel);
+    }
+}
